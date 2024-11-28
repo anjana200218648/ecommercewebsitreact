f1cb5e51931ba207ef061d8899c074e5af9e47af
H.H.H.Anjana <146448020+anjana200218648@users.noreply.github.com>
2024-11-28 23:04:03 +0530

    changers header

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,9 +9,9 @@ const Header = () => {
         <div className=''>
            <Logo w={80} h={60}/>
         </div>
-        <div className='flex items-center'>
-            <input type ='text' placeholder='Search product here.....'/>
-            <div className='text-lg'><IoIosSearch/></div>
+        <div className='hidden lg:flex items-center w-full justify-between max-w-sm border rounded-full focus-within:shadow-md pl-4'>
+            <input type ='text' placeholder='Search product here.....'className='w-full outline-none '/>
+            <div className='text-lg min-w-[50px] h-8 bg-red-700 flex items-center justify-center rounded-r-full text-white '><IoIosSearch/></div>
         </div>
 
         <div>
