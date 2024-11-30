17b1fdb8ff2dd0efada05df14aecfceaa9a189c0
H.H.H.Anjana <146448020+anjana200218648@users.noreply.github.com>
2024-11-30 11:19:36 +0530

    login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import loginicon from '../assest/signin.gif'
+import { FaEye } from "react-icons/fa";
+import { FaEyeSlash } from "react-icons/fa";
 
 const Login = () => {
+    const [showpassword,setpassword] = useState(false)
   return (
     <section id='login'>
-        <div className='max-auto container p-4'>
-            <div className='bg-white p-2 py-5 w-full max-w-md max-auto'>
+        <div className='max-auto container p-4 '>
+            <div className='bg-white p-2 py-5 w-full max-w-md mx-auto'>
                 <div className='w-20 h-20 mx-auto'>
                     <img src={loginicon} alt='login item'/>   
                 </div>
@@ -13,19 +16,34 @@ const Login = () => {
                 <form>
                     <div className='grid'>
                         <label>Email :   </label>
-                        <div>
-                        <input type='email' placeholder='enter email' className='w-full h-full outline-none'/>
+                        <div className='bg-slate-100 p-2'>
+                        <input type='email' placeholder='enter email' className='w-full h-full outline-none bg-transparent'/>
                         </div>
                     </div>
 
                     <div>
                         <label>Password :   </label>
-                       <div>
-                       <input type='password' placeholder='enter password' className='w-full h-full outline-none'/>
+                       <div className='bg-slate-100 flex p-2'>
+                       <input type={showpassword? "text":"password"} placeholder='enter password' className='w-full h-full outline-none  bg-transparent'/>
+                       <div className='cursor-pointer text-xl'onClick={()=>setpassword((preve)=>!preve)}>
+                        <span>
+                            {
+                                showpassword ?(
+                                    <FaEyeSlash/>
+                                )
+                                :
+                                (
+                                    <FaEye/>
+                                )
+                            }
+                            
+                            
+                        </span>
+                       </div>
                        </div>
                     </div>
 
-                    <button>Login </button>
+                    <button className='bg-red-600 text-white px-6 py-2 w-full max-w-[150px]'>Login </button>
                 </form>
 
             </div>
